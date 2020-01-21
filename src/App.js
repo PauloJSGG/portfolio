874d69116f313920ebd27804b2f2227cb936e886
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2020-01-21 00:15:51 +0000

    Add new idea

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import { useSpring, animated, config } from 'react-spring'
+import { render } from 'react-dom'
+import React, { useRef } from 'react'
+import clamp from 'lodash-es/clamp'
+import { useSprings, animated } from 'react-spring'
+import { useGesture } from 'react-use-gesture'
 import './App.css'
 
+const pages = [
+  'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/296878/pexels-photo-296878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+]
 
 function App() {
-  const [state, toggle] = useState(true)
-  const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: config.default, })
-  return (
-    <div className="app-container">
-      <div onClick={() => toggle(!state)}>
-        <animated.div
-          style={{
-            opacity: x.interpolate({ range: [0, 1], output: [0, 1] }),
-            transform: x
-              .interpolate({
-                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
-              })
-              .interpolate(x => `scale(${x})`)
-          }}>
-          wtfsdafsdfsad
-        </animated.div>
-      </div>
-    </div>
-  )
+  const index = useRef(0)
+  const [props, set] = useSprings(pages.length, i => ({ x: i * window.innerWidth, sc: 1, display: 'block' }))
+  const bind = useGesture(({ down, delta: [xDelta], direction: [xDir], distance, cancel }) => {
+    if (down && distance > window.innerWidth / 2)
+      cancel((index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, pages.length - 1)))
+    set(i => {
+      if (i < index.current - 1 || i > index.current + 1) return { display: 'none' }
+      const x = (i - index.current) * window.innerWidth + (down ? xDelta : 0)
+      const sc = down ? 1 - distance / window.innerWidth / 2 : 1
+      return { x, sc, display: 'block' }
+    })
+  })
+  return props.map(({ x, display, sc }, i) => (
+    <animated.div {...bind()} key={i} style={{ display, transform: x.interpolate(x => `translate3d(${x}px,0,0)`) }}>
+      <animated.div style={{ transform: sc.interpolate(s => `scale(${s})`), backgroundImage: `url(${pages[i]})` }} />
+    </animated.div>
+  ))
 }
 
-export default App;
+export default App
