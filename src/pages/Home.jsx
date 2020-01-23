5020bcd772cb07fec4ce7af1850b468a659c24a2
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2020-01-23 21:55:12 +0000

    Add animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useTrail, animated } from 'react-spring'
 import CasualPhoto from '../assets/Casual.JPG'
 import './Home.scss'
 
+const items = ['PAULO', 'GONÇALVES']
+const config = { mass: 50, tension: 2000, friction: 400 }
+
 const Home = (props) => {
+
+  const [toggle, set] = useState(true)
+  const trail = useTrail(items.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    x: toggle ? 0 : 20,
+    height: toggle ? 80 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+
   return (
     <div className='home-container'>
       <div className='header'>
-        <h1>PAULO GONÇALVES</h1>
+      {trail.map(({ x, height, ...rest }, index) => (
+          <animated.h1
+            key={items[index]}
+            className="trails-text"
+            style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+            <animated.div style={{ height }}>{items[index]}</animated.div>
+          </animated.h1>
+        ))}
       </div>
       <div className='body'>
         <img
